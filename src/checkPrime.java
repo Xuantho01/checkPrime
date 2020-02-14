ba67727177c701042c94c65741d0050383cd4150
Xuantho01 <hoxuantho95@gmail.com>
2020-02-14 11:13:00 +0700

    update

--- src/checkPrime.java
+++ src/checkPrime.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
 public class checkPrime {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter number to check: ");
+        int num = input.nextInt();
+
+        if(num < 2){
+            System.out.println(num+ " is not a prime");
+        }else{
+            int i = 2;
+            boolean flag = true;
+            while(i <= Math.sqrt(num)){
+                if(num % i == 0){
+                    flag = false;
+                    break;
+                }
+                i++;
+            }
+            if (flag)
+                System.out.println(num +" is a prime");
+            else
+                System.out.println(num +" is not a prime");
+        }
+    }
 }
